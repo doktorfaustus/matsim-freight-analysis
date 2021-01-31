47a615e2073b5fd6cce8ff482857f98be1a77512
Jakob F. Harnisch <git@jakobfaustus.de>
2021-01-31 16:56:31 +0100

    maven issues

--- src/main/java/org/matsim/project/FreightAnalysisShipmentTracking.java
+++ src/main/java/org/matsim/project/FreightAnalysisShipmentTracking.java
@@ -1,17 +1,32 @@
 package org.matsim.project;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.freight.carrier.CarrierShipment;
 import org.matsim.contrib.freight.events.ShipmentDeliveredEvent;
+import org.matsim.contrib.freight.events.ShipmentPickedUpEvent;
 
 import java.util.HashMap;
 
 public class FreightAnalysisShipmentTracking {
     HashMap<Id<CarrierShipment>, ShipmentTracker> shipments = new HashMap();
-    public void addTracker(CarrierShipment shipment){
+    public void addTracker(CarrierShipment shipment, Id<Person> driverId){
         shipments.put(shipment.getId(),new ShipmentTracker(shipment) );
     }
 
     public void trackEvent(ShipmentDeliveredEvent event) {
         shipments.get(event.getShipment().getId()).addEvent(event);
+    }
+
+    public void trackPickedUpEvent(ShipmentPickedUpEvent event) {
+        if (!shipments.containsKey(event.getShipment().getId())){
+            addTracker(event.getShipment(),event.getDriverId());
+        }
+
+    }
+
+    public void trackDeliveryEvent(ShipmentDeliveredEvent event) {
+    }
+
+    public void addTracker(CarrierShipment shipment) {
     }
 }
--- src/main/java/org/matsim/project/ShipmentTracker.java
+++ src/main/java/org/matsim/project/ShipmentTracker.java
@@ -7,6 +7,8 @@ import org.matsim.contrib.freight.carrier.Carrier;
 import org.matsim.contrib.freight.carrier.CarrierShipment;
 import org.matsim.core.utils.misc.Time;
 
+import java.sql.Driver;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class ShipmentTracker {
@@ -19,23 +21,20 @@ public class ShipmentTracker {
     private Boolean isOnTimePickUp;
     private Boolean isOnTimeDelivery;
     LinkedList<Event> events = new LinkedList<>();
+    HashSet<Id<Driver>> driverHistory = new HashSet<>();
 
-    public ShipmentTracker(CarrierShipment shipment){
-        this.id=shipment.getId();
+    public ShipmentTracker(CarrierShipment shipment) {
+        this.id = shipment.getId();
     }
-    public ShipmentTracker(CarrierShipment shipment, Id<Carrier> carrierId){
+
+    public ShipmentTracker(CarrierShipment shipment, Id<Carrier> carrierId) {
         this(shipment);
-        this.carrierId=carrierId;
+        this.carrierId = carrierId;
     }
-    public ShipmentTracker(CarrierShipment shipment, Id<Carrier> carrierId, Id<Person> driverId){
-       this(shipment, carrierId);
-       this.driverId = driverId;
 
-    }
-    public void addEvent(Event event){
-        events.add(event);
-    }
-    public LinkedList getEvents(){
-        return events;
+    public ShipmentTracker(CarrierShipment shipment, Id<Carrier> carrierId, Id<Person> driverId) {
+        this(shipment, carrierId);
+        this.driverId = driverId;
+
     }
 }
